2380b58acb680c90e74329355247540cd9970f49
arcodez <abelileitor123@gmail.com>
2022-02-24 23:11:51 -0400

    add js to found country

---
+++ country.js
@@ -0,0 +1,11 @@
+const url = 'https://ip.nf/me.json'
+
+// create a function to get the country from the url using fetch api
+
+const getCountry = async () => {
+    const response = await fetch(url)
+    const data = await response.json()
+    console.log(data.ip.country)
+}
+
+console.log(getCountry())
