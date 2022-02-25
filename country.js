2c79b93785c75bddb383903e6d3904e6cc19a28f
arcodez <abelileitor123@gmail.com>
2022-02-24 23:15:31 -0400

    update

--- country.js
+++ country.js
@@ -2,7 +2,7 @@ const url = 'https://ip.nf/me.json'
 
 // create a function to get the country from the url using fetch api
 
-const getCountry = async () => {
+const getCountry = async (url) => {
     const response = await fetch(url)
     const data = await response.json()
     console.log(data.ip.country)
